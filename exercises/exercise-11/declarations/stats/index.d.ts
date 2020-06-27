c65e2b34f12a14deb2abb8fda7feca9d5f7140cb
zhangjiaqi03 <zhangjiaqi03@baidu.com>
2020-06-27 17:58:47 +0800

    10-11

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,9 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export function getMaxIndex(input: array, comparator: function): number;
+    export function getMaxElement<T>(input: T, comparator: function): null | T[number];
+    export function getMinIndex(input: array, comparator: function): number;
+    export function getMinElement<T>(input: T, comparator: function): null | T[number];
+    export function getMedianIndex(input: array, comparator: function): number;
+    export function getMedianElement<T>(input: T, comparator: function): null | T[number];
+    export function getAverageValue(input: array, getValue: function): null | number;
 }
